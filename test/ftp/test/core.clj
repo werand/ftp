07b7af59f98bbd065b7a159fa4f1b7f03d2a58a1
Andreas Werner <werner_andreas@web.de>
2012-07-12 21:44:19 +0200

    Added example code.

--- test/ftp/test/core.clj
+++ test/ftp/test/core.clj
@@ -4,3 +4,29 @@
 
 (deftest replace-me ;; FIXME: write
   (is false "No tests have been written."))
+
+#_(def ^:dynamic *connection* {
+                 :url "192.168.178.21",
+                 :user "xxx",
+                 :password "xxx",
+                 :transfer-mode :binary})
+;; Transfer-mode may be one of :binary or :ascii if none is specified it will be :binary 
+
+; false
+#_(with-ftp *connection*
+  (exists-remotely? "some/file"))
+
+; true
+#_(with-ftp *connection*
+  (exists-remotely? "andreas/test.txt"))
+
+#_(with-ftp *connection*
+  (slurp "andreas/test.txt"))
+
+;; Binding of ftp-client to the ftp-client, so any method can be called on the client-api
+#_(with-ftp-client ftp-client *connection*
+  (.logout ftp-client)
+  (prn (exists-remotely? "some/file")))
+
+#_(with-ftp *connection*
+  (spit "andreas/test.txt" "This is a test"))
